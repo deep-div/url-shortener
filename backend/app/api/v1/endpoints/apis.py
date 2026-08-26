19f61087cab27df60b5bdeba768f1c4f009a173e
deep-div <div20023041@gmail.com>
2026-08-26 16:47:38 +0530

    import

--- backend/app/api/v1/endpoints/apis.py
+++ backend/app/api/v1/endpoints/apis.py
@@ -7,7 +7,7 @@ from app.clients.postgresql import get_db
 from app.modules.url_shortener.security import validate_url
 from app.modules.url_shortener.shorten import run_url_shortener, run_resolve_code
 from app.modules.url_analytics.analytics import get_url_stats as fetch_url_stats
-from app.modules.url_shortener.kafka_producer import produce_click_event
+from app.modules.url_shortener.kafka_producer_clicks import produce_click_event
 from app.modules.url_analytics.schema import UrlStatsResponse
 
 router = APIRouter()
