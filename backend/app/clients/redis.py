95120eae6737a8efcaf3a1c1da23a73cc20df238
deep-div <div20023041@gmail.com>
2026-08-27 16:44:44 +0530

    ssl error

--- backend/app/clients/redis.py
+++ backend/app/clients/redis.py
@@ -1,13 +1,14 @@
 from redis.asyncio import Redis, BlockingConnectionPool
+from redis.asyncio.connection import SSLConnection
 
 from app.core.config import settings
 
 redis_pool = BlockingConnectionPool(
+    connection_class=SSLConnection,
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     password=settings.REDIS_KEY,
     decode_responses=True,
-    ssl=True,
     max_connections=200,
     timeout=5,  # wait up to 5s for a free connection instead of raising immediately
 )
